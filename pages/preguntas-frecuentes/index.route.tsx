4b52a205a4db185b15203a262e177df3d143aa6f
yessid_vargas <yevasa93@gmail.com>
2024-03-16 21:48:38 -0500

    formula con faqs correctas

--- pages/preguntas-frecuentes/index.route.tsx
+++ pages/preguntas-frecuentes/index.route.tsx
@@ -67,6 +67,18 @@ const FaqsPage: NextPage<FaqsPageProps> = ({ faqs }) => {
   return (
     <LayoutGeneral>
       <div style={{ width: '70%', margin: '5rem' }}>
+
+
+      {faqs.map((faq) => (
+          <Accordion expanded={expanded === `panel${faq.id}`} onChange={handleChange(`panel${faq.id}`)} key={faq.id}>
+            <AccordionSummary aria-controls={`panel${faq.id}d-content`} id={`panel${faq.id}d-header`}>
+              <Typography>{faq.question}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>{faq.answer}</Typography>
+            </AccordionDetails>
+          </Accordion>
+        ))}
         <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
           <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
             <Typography>Collapsible Group Item #1</Typography>
@@ -111,13 +123,13 @@ const FaqsPage: NextPage<FaqsPageProps> = ({ faqs }) => {
   );
 }
 
-// export const getStaticProps: GetStaticProps = async (ctx) => {
-//   const faqs = await getFaqs();
-//   return {
-//     props: {
-//       faqs
-//     },
-//   }
-// }
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const faqs = await getFaqs();
+  return {
+    props: {
+      faqs
+    },
+  }
+}
 
 export default FaqsPage;
