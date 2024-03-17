d478d24c97cc906bbcb0556c780dc24caf15b807
yessid_vargas <yevasa93@gmail.com>
2024-03-16 21:55:13 -0500

    correccion2

--- services/getFaqs.ts
+++ services/getFaqs.ts
@@ -2,7 +2,7 @@ export const getFaqs = async () => {
     try {
          // cambiar ruta antes de hacer push para la construcción de vercel:
 		                                            //http://localhost:3000/api/faqs ----> en vercel seria ----> https://esp-fe-3-final-marvel.vercel.app/faqs
-        const response = await fetch('/api/faqs');
+        const response = await fetch('https://esp-fe-3-final-marvel.vercel.app/faqs');
         const data = await response.json();
         return data;
     } catch (error) {
